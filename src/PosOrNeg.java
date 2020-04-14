364bc3cfc95f66ba996bbc7df313638c66eefe36
hchau98 <chauhenry02@gmail.com>
2020-04-14 14:03:40 -0400

    First Commit

---
+++ src/PosOrNeg.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class PosOrNeg{
+    public static void main(String[] args){
+    Scanner input = new Scanner(System.in);
+    System.out.println("Enter a number: ");
+    int number = input.nextInt();
+    if(number>0)
+        System.out.println(number+" is positive");
+    else
+        System.out.println(number+" is negative");
+    }
+}
